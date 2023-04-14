a0a651af6b1628b0f7daed0fed55ef8b1d824728
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-04-14 09:54:26 +0300

    feat: digit & dream & extended tasks done

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let result = (n+'').split('').map((e, i, arr) => {
+    let newarr =  [...arr];
+    newarr.splice(i,1);
+    return Number(newarr.join(''))
+  })
+  return result.sort((a, b) => b - a)[0];
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( e, obj ) {
+  let newstr = (str, reptimes, separ, add, addtimes, addsepar) => { 
+    let noRepeatStr = str + '',
+        repeated = '',
+        separChecked = separ !== undefined ? separ : '+' ;
+        reptimesCheked = !reptimes && separChecked ? 1 : reptimes,
+        addtimesChecked = !addtimes && add ? 1 : addtimes,
+        addseparChecked = !addsepar && '|' || addsepar;
+        for(let i = 0; i<addtimesChecked; i++) {
+            noRepeatStr = noRepeatStr
+                                    .concat(add)
+                                    .concat(i+1 !== addtimesChecked ? addseparChecked : '');
+        }
+    for(let k = 0; k<reptimesCheked; k++) {
+      repeated = repeated
+                          .concat(noRepeatStr)
+                          .concat(k+1 !== reptimesCheked ? separChecked : '')
+    }
+    return repeated;
+  }
+  return newstr(e, obj.repeatTimes, obj.separator, obj.addition, obj.additionRepeatTimes, obj.additionSeparator)
 }
 
 module.exports = {
